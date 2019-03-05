6be250cf0535a013c76af990f14ddbaa4084888f
ahmad adawi <ahmad.adawi@intellisc.com>
2019-03-05 15:05:47 +0200

    json respinse

--- src/main/java/com/example/crud/controller/TestController.java
+++ src/main/java/com/example/crud/controller/TestController.java
@@ -1,13 +1,22 @@
 package com.example.crud.controller;
 
+import lombok.Data;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class TestController {
 
-    @GetMapping("test")
-    public String test(){
-        return "TEST Response ";
+    @GetMapping(value = "test",produces = MediaType.APPLICATION_JSON_VALUE)
+    public Test test(){
+        return new Test();
     }
+
+    @Data
+    class Test {
+        private String name = "Ahmad ";
+        private String age = "30";
+    }
+
 }
